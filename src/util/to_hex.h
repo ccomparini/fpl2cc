a7cb7b70dc0348132f33603830e6ba5ed1012c7e
chris <1158505+ccomparini@users.noreply.github.com>
2021-11-07 11:28:57 -0800

    don't include 0x at start of to_hex output
    ... so that callers can decide

--- src/util/to_hex.h
+++ src/util/to_hex.h
@@ -15,7 +15,8 @@ std::string to_hex(const T &inst) {
         '0', '1', '2', '3', '4', '5', '6', '7',
         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
     };
-    std::string out("0x");
+    //std::string out("0x");
+    std::string out;
     for(int ind = 0; ind < sizeof(T); ind++) {
        out += hchr[(hack.buf[ind] >> 4) & 0x0f];
        out += hchr[ hack.buf[ind]       & 0x0f];
