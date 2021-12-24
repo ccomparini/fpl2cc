986297b7e73b00a6c39347e91d912450954704c7
chris <1158505+ccomparini@users.noreply.github.com>
2021-12-24 12:37:36 -0800

    basic string to_hex template

--- src/util/to_hex.h
+++ src/util/to_hex.h
@@ -1,6 +1,7 @@
 #ifndef TO_HEX_H
 #define TO_HEX_H
 
+#include<array>
 #include<string>
 
 // why this not already exist?
@@ -15,11 +16,21 @@ std::string to_hex(const T &inst) {
         '0', '1', '2', '3', '4', '5', '6', '7',
         '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'
     };
-    //std::string out("0x");
+
     std::string out;
     for(int ind = 0; ind < sizeof(T); ind++) {
        out += hchr[(hack.buf[ind] >> 4) & 0x0f];
        out += hchr[ hack.buf[ind]       & 0x0f];
+    }
+    return out;
+}
+
+template<class TT>
+std::string bs_to_hex(const TT &in) {
+    std::string out;
+    for(auto el : in) {
+        out += to_hex(el);
+        out += ' ';
     }
     return out;
 }
