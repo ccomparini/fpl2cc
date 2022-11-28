af19a77de8f06e0c776c6b3ea032e8ea2b5eccd8
chris <1158505+ccomparini@users.noreply.github.com>
2022-11-27 20:27:31 -0800

    use asyncio instead of threads on interactive run_and_capture
    Because the subthreads which were supposed to read stdout and stderr
    would remain blocked on a read at the end of a process, the whole
    thing was cludged to make it so that the parent process would
    read anything remaining in the output streams after subprocess
    termination.  But, that creates a race between the parent process
    and the reader threads.  But, without the 2-place read there's
    a race between the end of the process and the reads.
    
    There were other possible fixes. Long story short, this is converted
    to asyncio and now seems to actually dtrt.  Moving on.

--- site_scons/site_init.py
+++ site_scons/site_init.py
@@ -1,9 +1,8 @@
+import asyncio
 import esml
 import os
 import pprint
-import signal
-import subprocess # requires python 3.x
-import threading
+import subprocess
 
 def run_and_capture_action(program, interactive=False):
     """
@@ -38,65 +37,57 @@ def run_and_capture_action(program, interactive=False):
     # contains the output of the parser or whatever we're testing,
     # but stderr may contain (among other things) interactive
     # debugging info and prompts.
-    # Returns a tuple of bytes containing whatever was written to
-    # (stdout, stderr).
-    def run_interactively(proc):
+    # Returns a tuple of bytes objects containing whatever the subproc
+    # wrote to stdout and stderr (respectively)
+    def run_interactively(command):
+        pout = b""
         perr = b""
-        stderr_done = False
-        def read_stderr():
-            nonlocal proc, perr, stderr_done
-            while not stderr_done:
-                inp = proc.stderr.read(1)
-                if len(inp) <= 0: # eof
-                    # cmc note: on my OSX box I can't seem to get here.
-                    stderr_done = True
-                else:
-                    # we use stderr interactively, so send it on directly
-                    # as well as saving it:
-                    sys.stderr.buffer.write(inp)
-                    sys.stderr.flush()
-                    perr += inp
+        returncode = -255
+        async def runit():
+            nonlocal command, pout, perr, returncode
+            proc = await asyncio.create_subprocess_exec(
+                *command,
+                stdin=sys.stdin,
+                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+            )
+            async def tee_stderr():
+                nonlocal proc, perr
+                done = False
+                while not done:
+                    inp = await proc.stderr.read(256)
+                    if len(inp) <= 0: # eof
+                        done = True
+                    else:
+                        # we use stderr interactively, so send it on directly
+                        # as well as saving it:
+                        sys.stderr.buffer.write(inp)
+                        sys.stderr.flush()
+                        perr += inp
 
-        # From the docs (https://docs.python.org/3/library/threading.html):
-        # "The significance of [the daemon flag] is that the entire Python
-        # program exits when only daemon threads are left."  (Note that the
-        # main thread isn't a daemon thread.)  Basically, it seems to mean
-        # that we don't have to join() it, which we is important because
-        # proc.stderr.read() seems to be blocking even when there's nothing
-        # to read (?), which means the sub never ends, and join() never
-        # returns.
-        # It's ok if the thread doesn't terminate normally - we should
-        # be able to read anything left over after the subproc terminates.
-        stderr_rdr = threading.Thread(target=read_stderr, daemon=True)
-        stderr_rdr.start()
+            async def read_stdout():
+                nonlocal proc, pout
+                done = False
+                while not done:
+                    inp = await proc.stdout.read(256) # 256 is an arbitrary buffer size
+                    if len(inp) <= 0: # eof
+                        done = True
+                    else:
+                        pout += inp
 
-        # No timeout if it's interactive - let the user take as long as
-        # they want.
-        proc.wait()
+            await asyncio.gather(read_stdout(), tee_stderr())
+            returncode = proc.returncode
 
-        # Finish reading anything we haven't yet read from stderr:
-        inp = proc.stderr.read()
-        if len(inp):
-            perr += inp
-            sys.stderr.buffer.write(inp)
-            sys.stderr.flush()
-
-        # Finally, deal with the proc's stdout:
-        pout = proc.stdout.read()
-        sys.stdout.buffer.write(pout)
-        sys.stdout.flush()
-
-        return pout, perr
+        asyncio.run(runit())
+        return returncode, pout, perr
 
     # runs the proc passed normally (blocking), and returns
     # a tuple with whatever was written to (stdout, stderr).
-    def run_normally(proc, timeout=5):
-        try:
-            proc.wait(timeout)
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            raise
-        return proc.stdout.read(), proc.stderr.read()
+    def run_normally(command, timeout=5):
+        proc = subprocess.Popen(command,
+            stdin=sys.stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        )
+        pout, perr = proc.communicate(timeout=timeout)
+        return proc.returncode, pout, perr
 
     # program should really be program + arguments, but support
     # just passing the name of the program:
@@ -127,24 +118,21 @@ def run_and_capture_action(program, interactive=False):
         command = [env.subst(p, target=target, source=source) for p in program]
 
         strcommand = ' '.join(command) # for error messages, etc.
-        proc = subprocess.Popen(command,
-            stdin=sys.stdin, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-        )
 
         if interactive:
-            pout, perr = run_interactively(proc)
+            returncode, pout, perr = run_interactively(command)
         else:
-            pout, perr = run_normally(proc)
+            returncode, pout, perr = run_normally(command)
 
-        if(proc.returncode < 0):
+        if(returncode < 0):
              # process died of some signal (interrupt, segfault, whatever):
-             signame = signal.Signals(-proc.returncode).name
-             msg = f"\n{signame} failure on command \"{strcommand}\"\n"
+             signame = signal.Signals(-returncode).name
+             msg = f"\n{signame} ({returncode}) failure on command \"{strcommand}\"\n"
              print(msg, file=sys.stderr)
 
         with open(capfile, mode='wb') as outf:
             outf.write(bytes(esml.dumps( {
-                    'returncode': proc.returncode,
+                    'returncode': returncode,
                     'stderr':     perr,
                     'stdout':     pout
                 }
