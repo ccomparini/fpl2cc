f594f4bac077c2d34620f27088baf9f5c702630c
chris <1158505+ccomparini@users.noreply.github.com>
2023-04-19 11:07:11 -0700

    util/join.h: add ability to pass format function
    You can now pass a format function to the join() function.  This
    is helpful for formatting things like std::map<>s, but allows
    arbitrary formatting of each element as well.

--- src/util/join.h
+++ src/util/join.h
@@ -2,10 +2,43 @@
 #define JOIN_H
 
 #include "is_iterable.h"
+#include "jerror.h"
+#include "src_location.h"
 #include "stringformat.h"
 
+#include <functional>
 #include <string>
 
+/*
+   join(iterable, join_value, formatter) -
+      returns a string containing each element formatted according to
+      the formatter passed, join using the join value string passed.
+ */
+template<typename T>
+std::string join(
+    T elements, const std::string &jv,
+    std::function<std::string(typename T::iterator)> fmtr,
+    src_location caller = CALLER()
+) {
+    if constexpr(!is_iterable(T)) {
+        jerror::error("type for first argument is not iterable", caller);
+    } else { 
+        std::string out;
+        for(auto el = elements.begin(); el != elements.end(); el++) {
+            if(out.length())
+                out += jv;
+            out += fmtr(el);
+        }
+        return out;
+    }
+    return "";
+}
+
+/*
+   join(iterable, join_value) - returns a string containing each element
+   of the iterable passed (formatted per stringformat()), with copies
+   of the join_value string between them.
+ */
 template<typename T>
 std::string join(T elements, const std::string &jv) {
     if constexpr(!is_iterable(T)) {
--- src/util/test/jointest.cc
+++ src/util/test/jointest.cc
@@ -1,20 +1,27 @@
 #include "join.h"
+#include "stringformat.h"
 
 #include <iostream>
 #include <list>
 #include <map>
 #include <string>
 
+std::string str_int_fmt(std::map<std::string, int>::iterator it) {
+    return stringformat("{} is like totally: {}", it->first, it->second);
+}
 
 int main() {
     std::list<std::string> clist = { "one", "two", "three", "four" };
     std::cout << join(clist, ", ")  << "\n";
     std::cout << join(clist, " x ") << "\n";
-/*
-    std::map<std::string, int> = {
-        { "foo", 23 }
+
+    std::map<std::string, int> kvs = {
+        { "foo", 23 },
+        { "bar", 55 },
+        { "bat", 63 }
     };
- */
+
+    std::cout << join(kvs, ", and another thing! ", str_int_fmt) << "\n";
 
     return 0;
 }
