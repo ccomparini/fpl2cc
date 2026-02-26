3f4d5835ea91f4ef1ad7f30f5dec95c55b833248
chris <1158505+ccomparini@users.noreply.github.com>
2026-02-26 09:20:19 -0800

    Refactor jerror with default handlers
    Default behaviour was handled in a special case.
    Adding default handlers (which do the same thing)
    in prep for allowing references to outer scope
    handlers.

--- src/util/jerror.h
+++ src/util/jerror.h
@@ -52,14 +52,39 @@ public:
     using callback = std::function<
         void(const std::string &error, src_location caller)
     >;
+    // this version lets the called function know which channel
+    // it's being called on, which allows implementors to use
+    // the same callback for different channels.  maybe overengineered.
     using callback_with_channel = std::function<
         void(channel chan, const std::string &error, src_location caller)
     >;
 
+    static void default_warning(channel chan, const std::string &msg, src_location caller = CALLER()) {
+        std::cerr << ensure_nl(stringformat("{} at {}", msg, caller));
+    }
+
+    static void default_error(channel chan, const std::string &msg, src_location caller = CALLER()) {
+        std::cerr << ensure_nl(stringformat("Fatal error: {}\tat {}", msg, caller));
+        exit(64);
+    }
+
 private:
     using handler_stack = std::list<callback_with_channel>;
+    // (struct handler_stacks is just a way to add default handlers)
+    struct handler_stacks {
+        handler_stack channels[NUM_CHANNELS];
 
-    static inline handler_stack channels[NUM_CHANNELS];
+        handler_stacks() {
+            channels[error_channel].push_back(default_error);
+            channels[warning_channel].push_back(default_warning);
+        }
+
+        handler_stack &operator [](int ind) {
+            return channels[ind];
+        }
+    };
+
+    static inline handler_stacks channels;
 
     static void on_msg(
         channel chan, const std::string &msg, src_location caller
@@ -69,12 +94,11 @@ private:
         if(handlers.size()) {
             (handlers.back())(chan, msg, caller);
         } else {
-            if(chan > error_channel) {
-                std::cerr << ensure_nl(stringformat("{} at {}", msg, caller));
-            } else {
-                std::cerr << ensure_nl(stringformat("Fatal error: {}\tat {}", msg, caller));
-                exit(2112);
-            }
+            std::cerr << ensure_nl(stringformat(
+                "Internal error: no message handler for channel {} "
+                "when trying to report \"{}\"\tfrom {}",
+                chan, msg, caller
+            ));
         }
     }
 
@@ -121,7 +145,6 @@ public:
         }
 
         ~handler() {
-
             pop_handler(which_chan);
         }
     };
