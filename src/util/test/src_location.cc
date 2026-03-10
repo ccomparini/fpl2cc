3d891df9896cd5f85c8f2e3371aa6f49cc0f6f3b
chris <1158505+ccomparini@users.noreply.github.com>
2026-03-09 19:52:12 -0700

    src_location tests

---
+++ src/util/test/src_location.cc
@@ -0,0 +1,21 @@
+
+#include "util/src_location.h"
+#include <iostream>
+#include <string>
+
+
+void print_caller(const std::string &msg, src_location caller = CALLER()) {
+    std::cout << msg << " from " << caller << "\n";
+}
+
+int main(int argc, char **argv) {
+
+    print_caller("\none");
+    print_caller("next line");
+
+
+    print_caller("skipped a couple");
+
+    print_caller("woot");
+}
+
